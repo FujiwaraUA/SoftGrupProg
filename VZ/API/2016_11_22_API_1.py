8354ef53a331425b7dc63d575e43c03f45e5bd82
SerkoUA <serko_x@ukr.net>
2016-11-28 00:15:04 +0200

    VZ - випускне завдання, розбираю API  сайт = openweathermap, місто = Тернопіль, на 5 днів

--- VZ/API/2016_11_22_API_1.py
+++ VZ/API/2016_11_22_API_1.py
@@ -5,7 +5,7 @@ city_id = 0
 appid = "1bb8b7a186b957c05745fd9c421fd44c"
 try:
     res = requests.get("http://api.openweathermap.org/data/2.5/find",
-                 params={'q': s_city, 'type': 'like', 'units': 'metric', 'APPID': appid})
+                 params={'q': s_city, 'type': 'like', 'units': 'metric', 'lang': 'uk', 'APPID': appid})
     data = res.json()
     cities = ["{} ({})".format(d['name'], d['sys']['country'])
               for d in data['list']]
@@ -16,24 +16,29 @@ except Exception as e:
     print("Exception (find):", e)
     pass
 
+print('#'* 50)
+'''
 try:
     res = requests.get("http://api.openweathermap.org/data/2.5/weather",
                  params={'id': city_id, 'units': 'metric', 'lang': 'uk', 'APPID': appid})
     data = res.json()
-    print("conditions:", data['weather'][0]['description'])
+    print("Характеристики погоди, атмосферні явища:", data['weather'][0]['description'])
     print("temp:", data['main']['temp'])
     print("temp_min:", data['main']['temp_min'])
     print("temp_max:", data['main']['temp_max'])
 except Exception as e:
     print("Exception (weather):", e)
     pass
-
+print('/' * 40)
+'''
 try:
     res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
-                       params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+                       params={'id': city_id, 'units': 'metric', 'lang': 'uk', 'APPID': appid})
+
     data = res.json()
     for i in data['list']:
         print(i['dt_txt'], '{0:+3.0f}'.format(i['main']['temp']), i['weather'][0]['description'])
 except Exception as e:
     print("Exception (forecast):", e)
     pass
+
